ec6c1c23669e213dfa3e41569b7dbd707d01bde2
Andrew G <55633473+geande@users.noreply.github.com>
2021-12-07 04:17:49 -0500

    Update gopro_perception.py
    Added April Tag Detection functionality.  Cleaned up functions for more general use.

--- perception/gopro_perception.py
+++ perception/gopro_perception.py
@@ -4,53 +4,87 @@ from time import time
 import socket
 from goprocam import GoProCamera
 from goprocam import constants
+from math import cos, pi
+import random as rng
 
 
-def edgedetection(frame):
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-    edges = cv2.Canny(gray, 10, 200)
-    return edges;
+from utils import *
+from AR_tags import *
 
-def gridlines(frame):
-    h, w, _ = frame.shape
-    rows, cols = (10, 10)
-    dy, dx = h / rows, w / cols
-    color = (0, 255, 0)
-    thickness = 1
-    for col in np.linspace(start=dx, stop=w-dx, num=cols-1):
-        col = int(round(col))
-        cv2.line(frame, (col, 0), (col, h), color=color, thickness=thickness)
+# Calibrated GoPro Camera Parameters (OpenCV)
+#K = np.array([[1425.5513881614268, 0.0, 1654.2719012237355], 
+#              [0.0, 1425.3478800534128, 1197.3579471903774], 
+#              [0.0, 0.0, 1.0]])
+#D = np.array([[0.09193360142523685], [0.14517625987376206], [-0.4878419565489222], [0.4019373729476832]])
 
-    for row in np.linspace(start=dy, stop=h-dy, num=rows-1):
-        row = int(round(row))
-        cv2.line(frame, (0, row), (w, row), color=color, thickness=thickness)
-    return frame;
+# Calibrated GoPro Camera Parameters (MATLAB)
+#K = np.array([[1224., 0.0, 3.6242e3], 
+#              [0.0, 1224., 2.5663e3], 
+#              [0.0, 0.0, 1.0]])
+#D = np.array([[1.3975e3], [-1.3595e-4], [-3.0898e-8], [4.0363e-13]])
+# MATLAB derived camera params for pinhole camera model ([fx, fy, cx, cy])
+#camera_params = [1224., 1224., 3.6242e3, 2.5663e3]
 
-def stream_cap(record):
+
+def stream_cap(augmented_reality, record, make_contours, val):
+    undistorter = UndistortionModule()
+    if augmented_reality:
+        plane_detector = AR_PlaneDetection()
     gpCam = GoProCamera.GoPro()
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    t=time()
     gpCam.livestream("start")
-    gpCam.video_settings(res='720p', fps='12')
-    gpCam.gpControlSet(constants.Stream.WINDOW_SIZE, constants.Stream.WindowSize.Default)
+    gpCam.video_settings(res='1080p', fps='12')
     gpCam.gpControlSet(constants.Stream.BIT_RATE, constants.Stream.BitRate.B2_4Mbps)
     cap = cv2.VideoCapture("udp://10.5.5.9:8554", cv2.CAP_FFMPEG)
-    t=time()
-    while True:
+    if record:
+        out = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (240,432))
+    t = time()
+    if make_contours:
+        cv2.namedWindow('OpenCV (Contours)')
+    else:
+        cv2.namedWindow('OpenCV (Edges)')
+    cv2.namedWindow('Undistorted')
+    counter = 0
+    while cap.isOpened():
         nmat, frame = cap.read()
-        canny = edgedetection(frame)
-        frame = gridlines(frame)
-        cv2.imshow("GoPro OpenCV", frame)
-        cv2.imshow("GoPro OpenCV (Edges)", canny)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-        if time() - t >= 2.5:
-            sock.sendto("_GPHD_:0:0:2:0.000000\n".encode(), ("10.5.5.9", 8554))
-            t=time()
+        if nmat:
+            if counter <= 3:
+                counter += 1
+                key = cv2.waitKey(15)
+            else:
+                undistorted = undistorter.undistort(frame)
+                if augmented_reality:
+                    pts_2d = plane_detector.detect_AprilTag(undistorted)
+                    if pts_2d is not None:
+                        for pt in pts_2d:
+                            cv2.circle(undistorted, pt, 3, (0, 0, 255), -1)
+                cv2.imshow('Undistorted', undistorted)
+
+                if make_contours:
+                    processed = contourdetection(undistorted, val)
+                    cv2.imshow('OpenCV (Contours)', processed)
+                else:
+                    processed = edgedetection(undistorted, val)
+                    cv2.imshow('OpenCV (Edges)', processed)
+                if record:
+                    out.write(processed)
+                key = cv2.waitKey(1)
+                counter = 0
+            if key == ord('q'):
+                break;
+            if time() - t >= 2.5:
+                sock.sendto("_GPHD_:0:0:2:0.000000\n".encode(), ("10.5.5.9", 8554))
+                t=time()
+        else:
+            break;
     # When everything is done, release the capture
     cap.release()
+    if record:
+        out.release()
     cv2.destroyAllWindows()
 
+augmented_reality = False
 record = False
-stream_cap(record)
+make_contours = False
+val = 20
+stream_cap(augmented_reality, record, make_contours, val)
